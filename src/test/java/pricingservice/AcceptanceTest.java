a832cfe0bbd7ae3bfcce3d2adeb564254f8cab9c
manikandan sadhasivam <manikandansadhasivam@manikandans-MacBook-Pro.local>
2018-11-09 07:44:51 +0800

    initial commit

--- src/test/java/pricingservice/AcceptanceTest.java
+++ src/test/java/pricingservice/AcceptanceTest.java
@@ -3,12 +3,19 @@ package pricingservice;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.embedder.Embedder;
+import org.jbehave.core.i18n.LocalizedKeywords;
 import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.junit.JUnitStory;
+import org.jbehave.core.reporters.CrossReference;
+import org.jbehave.core.reporters.FreemarkerViewGenerator;
 import org.jbehave.core.reporters.StoryReporterBuilder;
+
+import io.qameta.allure.jbehave.AllureJbehave;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
 
 import static java.util.stream.Collectors.toList;
 import static org.jbehave.core.reporters.Format.CONSOLE;
@@ -17,27 +24,41 @@ import static org.jbehave.core.reporters.Format.TXT;
 
 public abstract class AcceptanceTest extends JUnitStory {
 
-  public AcceptanceTest() {
-    Embedder embedder = configuredEmbedder();
-    embedder.useMetaFilters(getMetaFilters());
-    embedder.embedderControls().doVerboseFailures(true)
-            .useStoryTimeoutInSecs(60);
-  }
 
-  @Override
-  public Configuration configuration() {
-    return new MostUsefulConfiguration()
-            .useStoryReporterBuilder(new StoryReporterBuilder()
-                    .withDefaultFormats()
-                    .withFormats(CONSOLE, HTML, TXT)
-                    .withCodeLocation(
-                            CodeLocations.codeLocationFromPath("build/jbehave")));
-  }
+	@SuppressWarnings("deprecation")
+	public AcceptanceTest() {
+		Embedder embedder = configuredEmbedder();
+		embedder.useMetaFilters(getMetaFilters());
+		embedder
+		.embedderControls()
+		.doVerboseFailures(true)
+		.useStoryTimeoutInSecs(60);
+	}
 
-  private List<String> getMetaFilters() {
-    String metaFiltersProperty = System.getProperty("metaFilters", "");
-    return Arrays.stream(metaFiltersProperty.split(","))
-            .map(String::trim)
-            .collect(toList());
-  }
+	@Override
+	public Configuration configuration() {
+		final CrossReference xref = new CrossReference();
+		Properties viewResources = new Properties();
+		viewResources.setProperty("decorateNonHtml", "false");
+		return new MostUsefulConfiguration()
+				.useStoryReporterBuilder(new StoryReporterBuilder()
+						.withDefaultFormats().withFormats(CONSOLE, HTML, TXT)
+						.withViewResources(viewResources)
+						.withCrossReference(xref)
+						.withKeywords(new LocalizedKeywords(Locale.ENGLISH))
+						.withCodeLocation(CodeLocations.codeLocationFromPath("build/jbehave"))
+						.withReporters(new AllureJbehave())
+						)
+						
+				.useViewGenerator(new FreemarkerViewGenerator());
+	}
+
+	private List<String> getMetaFilters() {
+		String metaFiltersProperty = System.getProperty("metaFilters", "");
+		return Arrays
+				.stream(metaFiltersProperty
+				.split(","))
+				.map(String::trim)
+				.collect(toList());
+	}
 }
